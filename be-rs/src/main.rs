09fc99537284e3965114aaa6a3497e6d3dc1ba15
trevyn <trevyn-git@protonmail.com>
2021-07-25 17:24:25 +0400

    play with backend

--- be-rs/src/main.rs
+++ be-rs/src/main.rs
@@ -1,13 +1,12 @@
-use std::{env, io::Error};
-
 use futures_util::StreamExt;
-use log::info;
+// use tokio::io::AsyncWriteExt;
+// use log::info;
 use tokio::net::{TcpListener, TcpStream};
 
 #[tokio::main]
-async fn main() -> Result<(), Error> {
+async fn main() -> anyhow::Result<()> {
  let _ = env_logger::try_init();
- let addr = env::args().nth(1).unwrap_or_else(|| "127.0.0.1:8080".to_string());
+ let addr = "127.0.0.1:8080".to_string();
 
  // Create the event loop and TCP listener we'll accept connections on.
  let try_socket = TcpListener::bind(&addr).await;
@@ -32,5 +31,28 @@ async fn accept_connection(stream: TcpStream) {
  println!("New WebSocket connection: {}", addr);
 
  let (write, read) = ws_stream.split();
- read.forward(write).await.expect("Failed to forward message")
+
+ //  write
+ //   .send(tokio_tungstenite::tungstenite::protocol::Message::Text(
+ //    r#"{
+ //   "event": "ping",
+ //   "reqid": 42
+ // }"#
+ //     .to_string()
+ //     + "\n",
+ //   ))
+ //   .await
+ //   .unwrap();
+
+ //  println!("sent");
+
+ let read_future = read.for_each(|message| async {
+  let data = message.unwrap().into_data();
+  println!("received {} bytes: {:?}", data.len(), data);
+  // tokio::io::stdout().write(&data).await.unwrap();
+ });
+
+ read_future.await;
+
+ // read.forward(write).await.expect("Failed to forward message")
 }
