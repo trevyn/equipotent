d0dbcb21767c02e6362759276e310d5cc2369501
trevyn <trevyn-git@protonmail.com>
2021-07-26 16:34:21 +0400

    add lol_html

--- be-rs/src/main.rs
+++ be-rs/src/main.rs
@@ -6,6 +6,40 @@ use tokio::net::{TcpListener, TcpStream};
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
+ let req_url = format!("https://html.duckduckgo.com/html?q={}", "banana");
+ let agent = "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:73.0) Gecko/20100101 Firefox/73.0";
+
+ let client = reqwest::Client::new();
+ let req = client.get(&req_url).header(reqwest::header::USER_AGENT, agent);
+ let res = req.send().await?;
+ let bytes = res.bytes().await?;
+
+ let mut rewriter = lol_html::HtmlRewriter::new(
+  lol_html::Settings {
+   element_content_handlers: vec![
+    lol_html::element!("a[href]", |el| {
+     let href = el.get_attribute("href").expect("href was required");
+     println!("");
+     println!("----- {}", &href);
+     Ok(())
+    }),
+    lol_html::text!("a[href]", |el| {
+     print!("{}", el.as_str());
+     Ok(())
+    }),
+   ],
+   ..lol_html::Settings::default()
+  },
+  |_: &[u8]| {},
+ );
+
+ rewriter.write(bytes.as_ref())?;
+ rewriter.end()?;
+
+ // let links_main = Selector::parse(r#".links_main"#).unwrap();
+ // let result_snippet = Selector::parse(r#".result__snippet"#).unwrap();
+ // let result_a = Selector::parse(r#".result__a"#).unwrap();
+
  let mystruct = ResultItem { url: Some("imastring!!".to_string()), ..Default::default() };
  let serialized = bincode::serialize(&mystruct).unwrap();
  let deserialized: ResultItem = bincode::deserialize(&serialized).unwrap();
